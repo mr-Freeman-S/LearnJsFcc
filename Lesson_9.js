1baceceb5deb653909f6983c4f1ada843855316f
mr-Freeman-S <mr.baranok@mail.ru>
2022-01-03 21:00:10 +0300

    added lesson_9

---
+++ Lesson_9.js
@@ -0,0 +1,9 @@
+//Explore Differences Between the var and let Keywords
+
+//Перезапишет и не выдаст ошибку
+var catName = "Oliver";
+var catSound = "Meow!";
+
+//Выдаст ошибку и проще будет ее найти
+let camper = "James";
+let camper = "David";
