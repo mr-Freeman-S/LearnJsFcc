ca388cd400b9e7947477766438670f0cf538b6fe
mr-Freeman-S <mr.baranok@mail.ru>
2022-01-05 23:04:20 +0300

    added two lessons

---
+++ createFunction.js
@@ -0,0 +1,10 @@
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction()
+
+// funcrion with two parameters or arguments
+function functionWithArgs(numb1, numb2) {
+    console.log(numb1 + numb2);
+}
+functionWithArgs(2, 1)
---
+++ shoppingList.js
@@ -0,0 +1,8 @@
+const myList = [];
+
+myList.unshift(["Chocolate Bar", 15]);
+myList.push(["Cracker", 12]);
+myList.unshift(["Chocolate Bar", 15]);
+myList.push(["Cracker", 12]);
+myList.unshift(["Chocolate Bar", 15]);
+myList.push(["Cracker", 12]);
