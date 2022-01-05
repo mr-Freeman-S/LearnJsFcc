fd793fb4cb4e0fd575225efa59274cad5387a6dc
mr-Freeman-S <mr.baranok@mail.ru>
2022-01-05 23:58:29 +0300

    added new lessons

--- createFunction.js
+++ createFunction.js
@@ -8,3 +8,26 @@ function functionWithArgs(numb1, numb2) {
     console.log(numb1 + numb2);
 }
 functionWithArgs(2, 1)
+
+// function with return
+function timesFive(arg) {
+    return (arg * 5)
+}
+
+// function without return
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+    sum += 5
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
