237fd9efec409b8fb707814eefe4f102a4b2b1e0
mr-Freeman-S <mr.baranok@mail.ru>
2022-01-08 21:52:22 +0300

    added few lessons

---
+++ checkProp.js
@@ -0,0 +1,5 @@
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    return obj.hasOwnProperty(checkProp);
+    // Only change code above this line
+  }
--- objects.js
+++ objects.js
@@ -9,3 +9,26 @@ const testObj = {
   const entreeValue = testObj["an entree"];   // Change this line
   const drinkValue = testObj["the drink"];    // Change this line
 
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder";
+myDog.bark = "woof";
+delete myDog.tails
