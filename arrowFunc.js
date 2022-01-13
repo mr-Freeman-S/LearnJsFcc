3d95dc5b5be1780fb6523e3346287ea2445ccdff
mr-Freeman-S <mr.baranok@mail.ru>
2022-01-13 23:08:37 +0300

    add one lesson

--- arrowFunc.js
+++ arrowFunc.js
@@ -4,3 +4,8 @@ const magic = () => new Date();
 const myConcat = (arr1, arr2) => {
     return arr1.concat(arr2);
 };
+
+//set default parametr 
+// Only change code below this line
+const increment = (number = 0, value = 1) => number + value;
+// Only change code above this line
